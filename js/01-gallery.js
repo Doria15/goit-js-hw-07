719ff6dd20e3965bf5a218e0b039f9e665d08413
Doria15 <doriaceelin@gmail.com>
2023-11-01 19:55:07 +0800

    Updated task 1 and task 2

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryList = document.querySelector(".gallery");
+
+const createGallery = (gallery) => {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+
+`;
+    })
+    .join("");
+};
+
+const photosMarkup = createGallery(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", photosMarkup);
+
+const handleGalleryClick = (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const urlOriginal = e.target.dataset.source;
+
+  const instance = basicLightbox.create(`<img src="${urlOriginal}">`);
+  instance.show();
+
+  const handleOnEscKeyPress = (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", handleOnEscKeyPress);
+    }
+  };
+
+  window.addEventListener("keydown", handleOnEscKeyPress);
+};
+
+galleryList.addEventListener("click", handleGalleryClick);
